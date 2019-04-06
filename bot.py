160142421b0d9a0347d7f9a29ae907f0a88a888a
cartufer <31514298+cartufer@users.noreply.github.com>
2019-04-06 12:21:04 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,28 +1,34 @@
 import network, usocket, ussl, datetime, utime, os, json
 
-
+# try not to put things before the class, they won't be brought into your code when you reference the class
+# 
+# 
 # NETWORK_SETTINGS = 'settings.dat'
 # EMOTE_SETTINGS = 'emotes.txt'
 # max_buff = 1024
 # nick = ""
 # password = ""
 # chan = ""
-# chat_server = "irc-ws.chat.twitch.tv:80" // update to wss, cartufer
+# chat_server = "irc-ws.chat.twitch.tv:80" # update to wss, cartufer
 # wss = None
 class lumiere_bot
     NETWORK_SETTINGS = 'settings.dat'
-    EMOTE_SETTINGS = 'emotes.txt'
+    EMOTE_SETTINGS = 'emotes.txt' # write a handler for this file, cartufer
     max_buff = 1024
     # setup = False
     # nick = ""
     # password = ""
     # chan = ""
-    serveraddr = "irc-ws.chat.twitch.tv" // update to wss, cartufer
+    serveraddr = "irc-ws.chat.twitch.tv" # update to wss, cartufer
     # wss_server = "irc-ws.chat.twitch.tv"
     # wss = None
     sockaddr = None
-    wait = 1000
+    wait_time = 1000 # minimum time between messages in milliseconds, any less than a thousand and it's likely to just kick you
     def __init__(self, n = None, p = None, c = None, onmessage = None, onclose = None, wss_ssl = True):
+        # you can pass in the credentials through init and it'll just pass it to setup
+        # setup won't attempt to read from the .dat if setup is already done
+        # it won't attempt to connect until you tell it so, and then it'll run_loop
+        # 
         # self.nick = None
         # self.password = None
         # self.chan = None
@@ -39,22 +45,23 @@ class lumiere_bot
         # sock_connect()
         # run_loop()
 
-    def dummy():
-        pass
+    def dummy(self, dontcare):
+        pass # this is the dummy function for the callbacks, so it has a default value
+
     def wait():
-        n = utime.ticks_diff(ticks1, ticks2)
-        if n >= 0 and n < 1000:
-            utime.sleep_ms(ms)
+        n = utime.ticks_diff(ticks1, ticks2) # this function ensures we won't send messages too fast
+        if n >= 0 and n < self.wait_time: # make sure you wait atleast a second
+            utime.sleep_ms(self.wait_time - n) # this math is probably bad, longer is better
         self.lastmessage = utime.ticks_ms()
 
-    def post(self, message):
+    def post(self, message): # this does not yet support PRIVMSG to indivdual users and probably won't, poke me and i'll make it
         self.wait()
         if self.wss:
             self.wss_sock.write("PRIVMSG #" + self.chan + " :" + message + "\r\n")
         else:
             self.sock.send("PRIVMSG #" + self.chan + " :" + message + "\r\n")
 
-    def callbacks(self, onmessage, onclose):
+    def callbacks(self, onmessage, onclose): # you can use this function to configure callbacks if you setup credentials in a .dat
         self.onmessage = onmessage
         self.onclose = onclose
         print('callbacks setup, %s'.format(self))
@@ -64,7 +71,7 @@ class lumiere_bot
             self.nick = n
             self.password = p
             self.chan = c
-            self.wss = wss_ssl
+            self.wss = wss_ssl # is you are using this with a wifi device, please use wss/ssl, you'll thank me
             self.setup = True
             self.onmessage = onmessage
             self.onclose = onclose
@@ -80,9 +87,18 @@ class lumiere_bot
 
 
     def connect(self, n, p, c):
+        if n and p and c:
+            self.nick = n
+            self.password = p
+            self.chan = c
+            self.wss = True # if you are using this with a wifi device, please use wss/ssl, you'll thank me
+            self.setup = True
+            self.onmessage = self.dummy # if you have to you can even setup callbacks after you connect, not recommended
+            self.onclose = self.dummy
+        print('attempting connect, %s'.format(self)
         self.lastmessage = utime.ticks_ms()
         if !setup:
-            print('not setup, %s'.format(self))
+            print('not setup, aborting connect, %s'.format(self))
             return False
         if self.wss:
             self.sockaddr = usocket.getaddrinfo(serveraddr, 443)[0][-1]
@@ -94,10 +110,12 @@ class lumiere_bot
         if self.wss:
             self.wss_sock = ussl.wrap_socket(self.sock, server_side=False, keyfile=None, certfile=None, cert_reqs=CERT_NONE, ca_certs=None)
         # return False
+        # do some conection stuff here, cartufer
         run_loop()
 
-    def run_loop():
-        while True:
+    def run_loop(): # this function is where the magic happens, it is where most of the program happens
+        RUNNING = True
+        while RUNNING:
             try:
                 if self.wss:
                     reading = self.wss_sock.read(self.max_buff)
@@ -114,7 +132,7 @@ class lumiere_bot
                 else:
                     self.onmessage(reading)
             except (IOError, OSError) as err:
-                print("error found in run_loop, %s, %s".format(self, err))
+                print("error found while in run_loop, %s, %s".format(self, err))
                 onclose(err)
                 return err
 
@@ -143,7 +161,7 @@ class lumiere_bot
 
     def write_settings():
         self.setup = False
-        print('there is not yet a way to configure user settings, follow instructions on github page') // todo, cartufer
+        print('there is not yet a way to configure user settings, follow instructions on github page') # todo, cartufer
             # do something to make settings, or reset
             # os.remove("ChangedFile.csv")
 
