6569261d0cdc53652507115508acb8c269c6c016
cartufer <31514298+cartufer@users.noreply.github.com>
2019-04-11 19:39:01 -0500

    Update main.py

--- main.py
+++ main.py
@@ -10,11 +10,23 @@ if wlan is None:
 
 # Main Code goes here, wlan is a working network.WLAN(STA_IF) instance.
 print("ESP OK")
-def message_handler():
+
+# this file is going to be a sample implementation of bot.py
+# below are several related functions that users may find helpful
+
+def message_handler(message): # this will be the whole unmodified privmsg
+    
     pass
 
-def close_handler():
+def close_handler(thing1, thing2): # i forgot what these were, cartufer
+    pass
+
+def runloopcallback(): # this will be a place to put things to run inside of the bot's run loop
     pass
 
 lumi = lumiere_bot()
 lumi.callbacks(message_handler, close_handler)
+
+def is_sub_emote(word): # this function will eventually be able to evaluate if something is camelcase
+    if word[0] == word[0].lower() and word != word.lower(): # maybe this could be a bool lambda?
+        return True
